dc4c52988ce92747539c00a72df030fd2a3fb56c
Robson Ataíde <robsonataide@gmail.com>
2017-04-19 15:19:05 -0300

    Attach a type method when displayName is null.

--- src/main/java/desi/juan/internal/util/MethodGeneratorUtils.java
+++ src/main/java/desi/juan/internal/util/MethodGeneratorUtils.java
@@ -1,59 +1,57 @@
 /*
- * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * Copyright (c) MuleSoft, Inc. All rights reserved. http://www.mulesoft.com
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
 package desi.juan.internal.util;
 
-import com.google.common.base.CaseFormat;
-
 import javax.lang.model.SourceVersion;
 
-import desi.juan.internal.exception.IllegalNameException;
 import org.apache.commons.lang.StringUtils;
 import org.raml.model.Action;
 
-public class MethodGeneratorUtils
-{
+import com.google.common.base.CaseFormat;
 
-    public static String uriToCammelCase(String uri)
-    {
-        uri = uri.replace("{", "/").replace("}","/");
-        StringBuilder result = new StringBuilder();
-        for (String word : StringUtils.split(uri, "/"))
-        {
-            if (word.contains("_"))
-            {
-                word = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, word);
-            }
-            result.append(StringUtils.capitalize(word));
-        }
-        return StringUtils.uncapitalize(result.toString());
+import desi.juan.internal.exception.IllegalNameException;
+
+public class MethodGeneratorUtils {
+  
+  public static String uriToCammelCase(String uri) {
+    uri = uri.replace("{", "/").replace("}", "/");
+    StringBuilder result = new StringBuilder();
+    for(String word : StringUtils.split(uri, "/")) {
+      if(word.contains("_")) {
+        word = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, word);
+      }
+      result.append(StringUtils.capitalize(word));
     }
-
-    public static String buildMethodName(Action action)
-    {
-        return action.getDisplayName() != null ? action.getDisplayName() : uriToCammelCase(action.getResource().getUri());
+    return StringUtils.uncapitalize(result.toString());
+  }
+  
+  public static String buildMethodName(Action action) {
+    if(action.getDisplayName() != null)
+      return action.getDisplayName();
+    else {
+      String methodName = action.getResource().getUri() +
+              StringUtils.capitalize(StringUtils.lowerCase(action.getType().toString()));
+      return uriToCammelCase(methodName);
     }
-
-
-    public static String buildParamName(String name) throws IllegalNameException
-    {
-        if (!SourceVersion.isName(name))
-        {
-            name = name + "Value";
-            if(!SourceVersion.isName(name))
-            {
-                throw new IllegalNameException(name + " is not a valid java parameter name, update your RAML file so it can be parsed.");
-            }
-        }
-        return name;
+  }
+  
+  public static String buildParamName(String name) throws IllegalNameException {
+    if(!SourceVersion.isName(name)) {
+      name = name + "Value";
+      if(!SourceVersion.isName(name)) {
+        throw new IllegalNameException(
+                name + " is not a valid java parameter name, update your RAML file so it can be parsed.");
+      }
     }
-
-    public static String toStringValue(String value)
-    {
-        return "\"" + value + "\"";
-    }
-
+    return name;
+  }
+  
+  public static String toStringValue(String value) {
+    return "\"" + value + "\"";
+  }
+  
 }
